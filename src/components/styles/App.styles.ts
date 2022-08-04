f991febbb463a6362102ca5aa57c4c7a95ae87e8
Luan F. Silva <luanflorencio87@gmail.com>
2022-08-04 18:22:27 -0300

    style: Container component

---
+++ src/components/styles/App.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    @import url(./_variables.css);
+    width: 100%;
+    height: 100vh;
+    padding: 35px 50px;
+    background: var(--background);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
