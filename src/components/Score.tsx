f65565ce1c12e57bbbc03e9ed63700104ec993d3
Luan F. Silva <luanflorencio87@gmail.com>
2022-08-05 14:28:11 -0300

    add: Score

---
+++ src/components/Score.tsx
@@ -0,0 +1,12 @@
+import ScoreBar from "./styles/Score.styles";
+
+const Score = () => {
+    return (
+        <ScoreBar>
+            <p>SCORE</p>
+            <p id="points">12</p>
+        </ScoreBar>
+    )
+}
+
+export default Score
