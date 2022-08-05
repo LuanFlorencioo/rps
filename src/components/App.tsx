9ceb8ae62d3f383dc80e7a8adcd62e87d518b42b
Luan F. Silva <luanflorencio87@gmail.com>
2022-08-05 14:26:09 -0300

    feat: App update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,26 @@
-import { Container } from './styles/App.styles';
+import Logo from '../assets/Logo.svg';
+import Score from './Score';
+import Rules from './Rules';
+import { Container, Header, Main, Footer } from './styles/App.styles';
+
+import Box from './styles/Box.style';
 
 const App = () => {
   return (
     <Container>
+
+      <Header>
+        <img src={Logo} alt="Rock Paper Scissors" />
+        <Score />
+      </Header>
+
+      <Main>
+        <Box></Box>
+      </Main>
+
+      <Footer>
+        <Rules />
+      </Footer>
 
     </Container>
   )
