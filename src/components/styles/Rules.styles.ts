40ec128b58784ad6714b016ac87fafa6816dde11
Luan F. Silva <luanflorencio87@gmail.com>
2022-08-05 14:29:49 -0300

    add: style Rules

---
+++ src/components/styles/Rules.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const RulesButton = styled.button`
+    padding: 10px 30px;
+    background: transparent;
+    border: 1px solid #fff;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.8rem;
+    color: #fff;
+`
+
+export default RulesButton
